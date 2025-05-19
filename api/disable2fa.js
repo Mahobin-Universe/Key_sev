6ff7f9fb35ec950f9e786aee67897819d1caa4f8
Mahobin Universe <117584284+Mahobin-Universe@users.noreply.github.com>
2025-05-19 17:29:09 +0600

    Create disable2fa.js

---
+++ api/disable2fa.js
@@ -0,0 +1,17 @@
+import { KeyAuth } from '../../keyauth.js';
+
+const keyauth = new KeyAuth(process.env.APP_NAME, process.env.OWNER_ID, process.env.APP_VERSION);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST')
+    return res.status(405).json({ success: false, message: 'Method not allowed' });
+
+  const { token } = req.body;
+
+  try {
+    const result = await keyauth.disable2FA(token);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
