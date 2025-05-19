b524b66d6eff4916ff1583c057d158d47d161eb7
Mahobin Universe <117584284+Mahobin-Universe@users.noreply.github.com>
2025-05-19 17:21:17 +0600

    Create var.js

---
+++ api/var.js
@@ -0,0 +1,16 @@
+import { KeyAuth } from '../../keyauth.js';
+
+const keyauth = new KeyAuth(process.env.APP_NAME, process.env.OWNER_ID, process.env.APP_VERSION);
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).json({ success: false, message: 'Method not allowed' });
+
+  const varid = req.query.varid;
+
+  try {
+    const result = await keyauth.var(varid);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
