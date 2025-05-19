3f34cc45850cfc2276fedd27fe7a531a75cfcd08
Mahobin Universe <117584284+Mahobin-Universe@users.noreply.github.com>
2025-05-19 17:23:13 +0600

    Create getUserData.js

---
+++ api/getUserData.js
@@ -0,0 +1,15 @@
+import { KeyAuth } from '../../keyauth.js';
+
+const keyauth = new KeyAuth(process.env.APP_NAME, process.env.OWNER_ID, process.env.APP_VERSION);
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).json({ success: false, message: 'Method not allowed' });
+
+  try {
+    const result = await keyauth.getUserData();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
+
